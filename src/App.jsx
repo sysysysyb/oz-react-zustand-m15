bc9c65780100886cf235d0fca4cadc6dca35cfc6
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-09 17:54:03 +0900

    ✨ Feat: App에 DndContext 추가하고 dnd kit 속성 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { DndContext, PointerSensor, rectIntersection, useSensor, useSensors } from '@dnd-kit/core';
 import Boards from './components/Boards';
 import Controller from './components/Controller';
 
@@ -5,24 +6,52 @@ import Controller from './components/Controller';
 //2. Recoil과 관련된 데이터를 삭제하고, RecoilRoot를 제거하세요.
 
 function App() {
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    })
+  );
+
+  const handleDragStart = (event) => {
+    console.log('drag start');
+  };
+
+  const handleDragEnd = (event) => {
+    console.log('drag end');
+  };
+
+  const handleDragOver = (event) => {
+    console.log('drag over');
+  };
+
   return (
-    <div className="flex flex-col h-screen">
-      <header className="w-full h-[80px] bg-slate-800 flex flex-col items-center justify-center text-stone-100">
-        <p className="text-lg font-semibold">Kanban Board Project</p>
-        <p>Chapter 2. Zustand</p>
-      </header>
-      <main className="flex-1 flex flex-col justify-between">
-        <div className="grid grid-cols-3 gap-4 p-4 w-full">
-          <Boards type={'todo'} />
-          <Boards type={'inprogress'} />
-          <Boards type={'done'} />
-        </div>
-        <Controller />
-      </main>
-      <footer className="w-full h-[60px] bg-slate-800 flex items-center text-stone-100 justify-center">
-        <p>&copy; OZ-CodingSchool</p>
-      </footer>
-    </div>
+    <DndContext
+      sensors={sensors}
+      collisionDetection={rectIntersection}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
+    >
+      <div className="flex flex-col h-screen">
+        <header className="w-full h-[80px] bg-slate-800 flex flex-col items-center justify-center text-stone-100">
+          <p className="text-lg font-semibold">Kanban Board Project</p>
+          <p>Chapter 2. Zustand</p>
+        </header>
+        <main className="flex-1 flex flex-col justify-between">
+          <div className="grid grid-cols-3 gap-4 p-4 w-full">
+            <Boards type={'todo'} />
+            <Boards type={'inprogress'} />
+            <Boards type={'done'} />
+          </div>
+          <Controller />
+        </main>
+        <footer className="w-full h-[60px] bg-slate-800 flex items-center text-stone-100 justify-center">
+          <p>&copy; OZ-CodingSchool</p>
+        </footer>
+      </div>
+    </DndContext>
   );
 }
 
