7a0d0485646ee7f1fe41646b1b2e79c899ede63a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-09 18:28:06 +0900

    ✨ Feat: board를 드래그 앤 드롭할 때의 스타일을 DragOverlay에 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { DndContext, PointerSensor, rectIntersection, useSensor, useSensors } from '@dnd-kit/core';
+import { DndContext, DragOverlay, PointerSensor, rectIntersection, useSensor, useSensors } from '@dnd-kit/core';
 import Boards from './components/Boards';
 import Controller from './components/Controller';
+import useBoardStore from './stores/store';
+import { useState } from 'react';
 
 //1. zustand로 상태 관리 라이브러리를 변경해야 합니다.
 //2. Recoil과 관련된 데이터를 삭제하고, RecoilRoot를 제거하세요.
 
 function App() {
+  const { board, updateBoardType, reorderItems } = useBoardStore();
+  const [activeId, setActiveId] = useState(null);
+  const activeItem = activeId ? board.find((item) => item.id === activeId) : null;
+
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: {
@@ -51,6 +57,21 @@ function App() {
           <p>&copy; OZ-CodingSchool</p>
         </footer>
       </div>
+      <DragOverlay>
+        {activeId && activeItem && (
+          <div className="bg-white shadow-xl rounded-md p-4 w-full">
+            <div className="flex items-center justify-between">
+              <h3 className="text-lg font-semibold">{activeItem.title}</h3>
+              {activeItem.type === 'todo' && <div className="animate-pulse w-2 h-2 rounded-full bg-green-500"></div>}
+              {activeItem.type === 'inprogress' && (
+                <div className="animate-pulse w-2 h-2 rounded-full bg-amber-500"></div>
+              )}
+              {activeItem.type === 'done' && <div className="animate-pulse w-2 h-2 rounded-full bg-red-500"></div>}
+            </div>
+            <p className="text-sm text-gray-500">{activeItem.created_at}</p>
+          </div>
+        )}
+      </DragOverlay>
     </DndContext>
   );
 }
