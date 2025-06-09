eb658824ffe4ccbc4201d323c220747d583c6d4c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-09 17:19:13 +0900

    ✨ Feat: persist와 createJSONStorage를 이용해서 로컬 스토리지에 데이터 저장

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,19 +1,28 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
-const useBoardStore = create((set) => ({
-  board: [],
-  addBoard: (newBoard) => set((state) => ({ board: [...state.board, newBoard] })),
-  removeBoard: (id) => set((state) => ({ board: state.board.filter((b) => b.id !== id) })),
-  updateBoard: (item) =>
-    set((state) => ({
-      board: state.board.map((b) => {
-        if (b.id === item.id) {
-          return item;
-        } else {
-          return b;
-        }
-      }),
-    })),
-}));
+const useBoardStore = create(
+  persist(
+    (set) => ({
+      board: [],
+      addBoard: (newBoard) => set((state) => ({ board: [...state.board, newBoard] })),
+      removeBoard: (id) => set((state) => ({ board: state.board.filter((b) => b.id !== id) })),
+      updateBoard: (item) =>
+        set((state) => ({
+          board: state.board.map((b) => {
+            if (b.id === item.id) {
+              return item;
+            } else {
+              return b;
+            }
+          }),
+        })),
+    }),
+    {
+      name: 'board-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useBoardStore;
